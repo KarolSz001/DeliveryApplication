65007e5dd8a2c53d7769fd8a70a7959b7354c441
karol_szot <karol_szot@o2.pl>
2020-08-23 21:44:55 +0100

    reconfiguration compiler anf maven properties

--- main/src/main/java/com/app/App.java
+++ main/src/main/java/com/app/App.java
@@ -8,6 +8,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        var sb = new StringBuilder();
+        sb.append(" ----------------------------------------------------------------------------- \n");
+        sb.append(" DeliveryApplication v1.0 23.08.2020 \n ");
+        sb.append(" Karol Szot \n");
+        sb.append(" ----------------------------------------------------------------------------- \n");
+        System.out.println(sb.toString());
+
     }
 }
